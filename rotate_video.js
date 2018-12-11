d242c244dc2e21c42ec32615afa15bb31099d571
eliglazier <eliglazier@gmail.com>
2018-12-11 11:04:09 -0500

    Add files via upload

---
+++ rotate_video.js
@@ -0,0 +1,820 @@
+var pjson  = [
+  {
+    "x": -76.935119396,
+    "y": 39.010165008,
+    "sec": 0,
+    "dir": 45
+  },
+  {
+    "x": -76.935100537,
+    "y": 39.010158554,
+    "sec": 1,
+    "dir": 45
+  },
+  {
+    "x": -76.935080756,
+    "y": 39.010149669,
+    "sec": 2,
+    "dir": 45
+  },
+  {
+    "x": -76.935053263,
+    "y": 39.010137599,
+    "sec": 3,
+    "dir": 45
+  },
+  {
+    "x": -76.935011018,
+    "y": 39.010129049,
+    "sec": 4,
+    "dir": 45
+  },
+  {
+    "x": -76.934965756,
+    "y": 39.010118237,
+    "sec": 5,
+    "dir": 45
+  },
+  {
+    "x": -76.934930971,
+    "y": 39.010110442,
+    "sec": 6,
+    "dir": 45
+  },
+  {
+    "x": -76.934899958,
+    "y": 39.010105748,
+    "sec": 7,
+    "dir": 45
+  },
+  {
+    "x": -76.934869112,
+    "y": 39.010111783,
+    "sec": 8,
+    "dir": 45
+  },
+  {
+    "x": -76.934844889,
+    "y": 39.010130642,
+    "sec": 9,
+    "dir": 45
+  },
+  {
+    "x": -76.934806667,
+    "y": 39.010144556,
+    "sec": 10,
+    "dir": 45
+  },
+  {
+    "x": -76.934758387,
+    "y": 39.010158805,
+    "sec": 11,
+    "dir": 45
+  },
+  {
+    "x": -76.934711952,
+    "y": 39.010175653,
+    "sec": 12,
+    "dir": 45
+  },
+  {
+    "x": -76.9346656,
+    "y": 39.010188896,
+    "sec": 13,
+    "dir": 45
+  },
+  {
+    "x": -76.93461447,
+    "y": 39.01020214,
+    "sec": 14,
+    "dir": 45
+  },
+  {
+    "x": -76.934562167,
+    "y": 39.010210605,
+    "sec": 15,
+    "dir": 45
+  },
+  {
+    "x": -76.934508272,
+    "y": 39.010220244,
+    "sec": 16,
+    "dir": 45
+  },
+  {
+    "x": -76.934462003,
+    "y": 39.01023047,
+    "sec": 17,
+    "dir": 45
+  },
+  {
+    "x": -76.934416825,
+    "y": 39.01024078,
+    "sec": 18,
+    "dir": 45
+  },
+  {
+    "x": -76.934373155,
+    "y": 39.010251593,
+    "sec": 19,
+    "dir": 45
+  },
+  {
+    "x": -76.934327558,
+    "y": 39.010261064,
+    "sec": 20,
+    "dir": 45
+  },
+  {
+    "x": -76.934291767,
+    "y": 39.010277744,
+    "sec": 21,
+    "dir": 45
+  },
+  {
+    "x": -76.934248852,
+    "y": 39.01028512,
+    "sec": 22,
+    "dir": 45
+  },
+  {
+    "x": -76.934195124,
+    "y": 39.010284869,
+    "sec": 23,
+    "dir": 45
+  },
+  {
+    "x": -76.934125219,
+    "y": 39.010288389,
+    "sec": 24,
+    "dir": 45
+  },
+  {
+    "x": -76.93407476,
+    "y": 39.010303393,
+    "sec": 25,
+    "dir": 45
+  },
+  {
+    "x": -76.934022875,
+    "y": 39.010319319,
+    "sec": 26,
+    "dir": 45
+  },
+  {
+    "x": -76.933977362,
+    "y": 39.010329125,
+    "sec": 27,
+    "dir": 45
+  },
+  {
+    "x": -76.933929501,
+    "y": 39.010333903,
+    "sec": 28,
+    "dir": 45
+  },
+  {
+    "x": -76.933880299,
+    "y": 39.010342453,
+    "sec": 29,
+    "dir": 45
+  },
+  {
+    "x": -76.933827745,
+    "y": 39.01035184,
+    "sec": 30,
+    "dir": 45
+  },
+  {
+    "x": -76.93377519,
+    "y": 39.01035402,
+    "sec": 31,
+    "dir": 45
+  },
+  {
+    "x": -76.933723306,
+    "y": 39.010353601,
+    "sec": 32,
+    "dir": 45
+  },
+  {
+    "x": -76.933678966,
+    "y": 39.010359636,
+    "sec": 33,
+    "dir": 45
+  },
+  {
+    "x": -76.933631441,
+    "y": 39.010360558,
+    "sec": 34,
+    "dir": 45
+  },
+  {
+    "x": -76.933588022,
+    "y": 39.010361144,
+    "sec": 35,
+    "dir": 45
+  },
+  {
+    "x": -76.933538402,
+    "y": 39.010367682,
+    "sec": 36,
+    "dir": 45
+  },
+  {
+    "x": -76.933491882,
+    "y": 39.010366676,
+    "sec": 37,
+    "dir": 45
+  },
+  {
+    "x": -76.933445781,
+    "y": 39.010365168,
+    "sec": 38,
+    "dir": 45
+  },
+  {
+    "x": -76.933394736,
+    "y": 39.010360474,
+    "sec": 39,
+    "dir": 45
+  },
+  {
+    "x": -76.93331209,
+    "y": 39.010350332,
+    "sec": 40,
+    "dir": 45
+  },
+  {
+    "x": -76.933259368,
+    "y": 39.010342536,
+    "sec": 41,
+    "dir": 45
+  },
+  {
+    "x": -76.933214441,
+    "y": 39.010339938,
+    "sec": 42,
+    "dir": 45
+  },
+  {
+    "x": -76.933170687,
+    "y": 39.010323593,
+    "sec": 43,
+    "dir": 45
+  },
+  {
+    "x": -76.933129951,
+    "y": 39.010322252,
+    "sec": 44,
+    "dir": 45
+  },
+  {
+    "x": -76.933081169,
+    "y": 39.010320827,
+    "sec": 45,
+    "dir": 45
+  },
+  {
+    "x": -76.933025764,
+    "y": 39.01031781,
+    "sec": 46,
+    "dir": 45
+  },
+  {
+    "x": -76.932980837,
+    "y": 39.010300711,
+    "sec": 47,
+    "dir": 45
+  },
+  {
+    "x": -76.932934821,
+    "y": 39.010280427,
+    "sec": 48,
+    "dir": 45
+  },
+  {
+    "x": -76.932871118,
+    "y": 39.010276068,
+    "sec": 49,
+    "dir": 45
+  },
+  {
+    "x": -76.932823928,
+    "y": 39.010289228,
+    "sec": 50,
+    "dir": 45
+  },
+  {
+    "x": -76.932782941,
+    "y": 39.010297609,
+    "sec": 51,
+    "dir": 45
+  },
+  {
+    "x": -76.932744384,
+    "y": 39.010304483,
+    "sec": 52,
+    "dir": 45
+  },
+  {
+    "x": -76.932711694,
+    "y": 39.010328203,
+    "sec": 53,
+    "dir": 45
+  },
+  {
+    "x": -76.932685208,
+    "y": 39.010348655,
+    "sec": 54,
+    "dir": 45
+  },
+  {
+    "x": -76.932666935,
+    "y": 39.010381596,
+    "sec": 55,
+    "dir": 45
+  },
+  {
+    "x": -76.932650004,
+    "y": 39.010411687,
+    "sec": 56,
+    "dir": 45
+  },
+  {
+    "x": -76.932637096,
+    "y": 39.010439096,
+    "sec": 57,
+    "dir": 45
+  },
+  {
+    "x": -76.932620583,
+    "y": 39.010469355,
+    "sec": 58,
+    "dir": 45
+  },
+  {
+    "x": -76.932591079,
+    "y": 39.010502212,
+    "sec": 59,
+    "dir": 45
+  },
+  {
+    "x": -76.932554785,
+    "y": 39.010536829,
+    "sec": 60,
+    "dir": 45
+  },
+  {
+    "x": -76.932517234,
+    "y": 39.010579325,
+    "sec": 61,
+    "dir": 45
+  },
+  {
+    "x": -76.932486473,
+    "y": 39.010619223,
+    "sec": 62,
+    "dir": 45
+  },
+  {
+    "x": -76.932458729,
+    "y": 39.010643028,
+    "sec": 63,
+    "dir": 45
+  },
+  {
+    "x": -76.932437606,
+    "y": 39.010666916,
+    "sec": 64,
+    "dir": 45
+  },
+  {
+    "x": -76.932406761,
+    "y": 39.01069575,
+    "sec": 65,
+    "dir": 45
+  },
+  {
+    "x": -76.932380777,
+    "y": 39.010738833,
+    "sec": 66,
+    "dir": 45
+  },
+  {
+    "x": -76.932341298,
+    "y": 39.01075962,
+    "sec": 67,
+    "dir": 45
+  },
+  {
+    "x": -76.932298215,
+    "y": 39.010790801,
+    "sec": 68,
+    "dir": 45
+  },
+  {
+    "x": -76.932275165,
+    "y": 39.010824664,
+    "sec": 69,
+    "dir": 45
+  },
+  {
+    "x": -76.93223116,
+    "y": 39.010858778,
+    "sec": 70,
+    "dir": 45
+  },
+  {
+    "x": -76.932194615,
+    "y": 39.010881325,
+    "sec": 71,
+    "dir": 45
+  },
+  {
+    "x": -76.932157818,
+    "y": 39.010903202,
+    "sec": 72,
+    "dir": 45
+  },
+  {
+    "x": -76.932134768,
+    "y": 39.01092709,
+    "sec": 73,
+    "dir": 45
+  },
+  {
+    "x": -76.93209663,
+    "y": 39.010943351,
+    "sec": 74,
+    "dir": 45
+  },
+  {
+    "x": -76.932070144,
+    "y": 39.010962965,
+    "sec": 75,
+    "dir": 45
+  },
+  {
+    "x": -76.932038628,
+    "y": 39.010989368,
+    "sec": 76,
+    "dir": 45
+  },
+  {
+    "x": -76.932007112,
+    "y": 39.011014011,
+    "sec": 77,
+    "dir": 45
+  },
+  {
+    "x": -76.93196537,
+    "y": 39.011047287,
+    "sec": 78,
+    "dir": 45
+  },
+  {
+    "x": -76.9319419,
+    "y": 39.011078384,
+    "sec": 79,
+    "dir": 45
+  },
+  {
+    "x": -76.931918012,
+    "y": 39.0111042,
+    "sec": 80,
+    "dir": 45
+  },
+  {
+    "x": -76.931888172,
+    "y": 39.011136638,
+    "sec": 81,
+    "dir": 45
+  },
+  {
+    "x": -76.931845844,
+    "y": 39.011165472,
+    "sec": 82,
+    "dir": 45
+  },
+  {
+    "x": -76.931812903,
+    "y": 39.011194557,
+    "sec": 83,
+    "dir": 45
+  },
+  {
+    "x": -76.931784991,
+    "y": 39.011214087,
+    "sec": 84,
+    "dir": 45
+  },
+  {
+    "x": -76.931751966,
+    "y": 39.011233114,
+    "sec": 85,
+    "dir": 45
+  },
+  {
+    "x": -76.931714667,
+    "y": 39.011254571,
+    "sec": 86,
+    "dir": 45
+  },
+  {
+    "x": -76.931670159,
+    "y": 39.011273263,
+    "sec": 87,
+    "dir": 45
+  },
+  {
+    "x": -76.931631602,
+    "y": 39.011299498,
+    "sec": 88,
+    "dir": 45
+  },
+  {
+    "x": -76.931580892,
+    "y": 39.011328416,
+    "sec": 89,
+    "dir": 45
+  },
+  {
+    "x": -76.931546107,
+    "y": 39.011356831,
+    "sec": 90,
+    "dir": 45
+  },
+  {
+    "x": -76.9315104,
+    "y": 39.011381976,
+    "sec": 91,
+    "dir": 45
+  },
+  {
+    "x": -76.931472682,
+    "y": 39.011404691,
+    "sec": 92,
+    "dir": 45
+  },
+  {
+    "x": -76.931445608,
+    "y": 39.011425897,
+    "sec": 93,
+    "dir": 45
+  },
+  {
+    "x": -76.931414176,
+    "y": 39.011446182,
+    "sec": 94,
+    "dir": 45
+  },
+  {
+    "x": -76.931381235,
+    "y": 39.011460682,
+    "sec": 95,
+    "dir": 45
+  },
+  {
+    "x": -76.931354497,
+    "y": 39.011473926,
+    "sec": 96,
+    "dir": 45
+  },
+  {
+    "x": -76.931318538,
+    "y": 39.011483313,
+    "sec": 97,
+    "dir": 45
+  },
+  {
+    "x": -76.931283669,
+    "y": 39.011493707,
+    "sec": 98,
+    "dir": 45
+  },
+  {
+    "x": -76.931249471,
+    "y": 39.011496222,
+    "sec": 99,
+    "dir": 45
+  },
+  {
+    "x": -76.931202197,
+    "y": 39.011494545,
+    "sec": 100,
+    "dir": 45
+  },
+  {
+    "x": -76.93116582,
+    "y": 39.011496389,
+    "sec": 101,
+    "dir": 45
+  },
+  {
+    "x": -76.931118546,
+    "y": 39.01150628,
+    "sec": 102,
+    "dir": 45
+  },
+  {
+    "x": -76.931084264,
+    "y": 39.011508375,
+    "sec": 103,
+    "dir": 45
+  },
+  {
+    "x": -76.931054508,
+    "y": 39.011513321,
+    "sec": 104,
+    "dir": 45
+  },
+  {
+    "x": -76.93102425,
+    "y": 39.011519859,
+    "sec": 105,
+    "dir": 45
+  },
+  {
+    "x": -76.930995416,
+    "y": 39.011527067,
+    "sec": 106,
+    "dir": 45
+  },
+  {
+    "x": -76.930962056,
+    "y": 39.011532431,
+    "sec": 107,
+    "dir": 45
+  },
+  {
+    "x": -76.930935485,
+    "y": 39.011531509,
+    "sec": 108,
+    "dir": 45
+  },
+  {
+    "x": -76.930900784,
+    "y": 39.011534946,
+    "sec": 109,
+    "dir": 45
+  },
+  {
+    "x": -76.930868849,
+    "y": 39.011540646,
+    "sec": 110,
+    "dir": 45
+  },
+  {
+    "x": -76.930831634,
+    "y": 39.011547184,
+    "sec": 111,
+    "dir": 45
+  },
+  {
+    "x": -76.930799279,
+    "y": 39.011552045,
+    "sec": 112,
+    "dir": 45
+  },
+  {
+    "x": -76.930770362,
+    "y": 39.011556823,
+    "sec": 113,
+    "dir": 45
+  },
+  {
+    "x": -76.93074815,
+    "y": 39.011585154,
+    "sec": 114,
+    "dir": 45
+  },
+  {
+    "x": -76.930724848,
+    "y": 39.011589931,
+    "sec": 115,
+    "dir": 45
+  }
+]
+;
+
+L.mapbox.accessToken = 'pk.eyJ1IjoidG9vbGVkZXNpZ24iLCJhIjoiNTcyZTIwNmExODVmYTAzZDA0NWQ0YTFlNTA1YWMyZjcifQ.5bYQWlwCYVUnqJFPTx1DWg';
+var map = L.mapbox.map('map', 'mapbox.streets').setView([39.011585154,-76.93074815], 17);
+
+var video = $('#video');
+
+// build line vector based on json points
+var point_locs = [];
+for (var i = 0, e = pjson.length; i < e; ++i) {
+	point_locs[i] = [pjson[i].x, pjson[i].y];
+}
+var line_geojson = {
+	type: "Feature",
+	geometry: {type: "LineString", coordinates: point_locs},
+	properties: {"stroke": "#fc4353", "stroke-width": 5}
+};
+L.geoJson(line_geojson, { style: L.mapbox.simplestyle.style }).addTo(map);
+
+// Rotating and controllable marker code by Benjamin Becquet
+// https://github.com/bbecquet/Leaflet.PolylineDecorator
+L.RotatedMarker = L.Marker.extend({
+  options: { angle: 0 },
+  _setPos: function(pos) {
+	L.Marker.prototype._setPos.call(this, pos);
+	if (L.DomUtil.TRANSFORM) {
+	  // use the CSS transform rule if available
+	  this._icon.style[L.DomUtil.TRANSFORM] += ' rotate(' + this.options.angle + 'deg)';
+	} else if (L.Browser.ie) {
+	  // fallback for IE6, IE7, IE8
+	  var rad = this.options.angle * L.LatLng.DEG_TO_RAD,
+	  costheta = Math.cos(rad),
+	  sintheta = Math.sin(rad);
+	  this._icon.style.filter += ' progid:DXImageTransform.Microsoft.Matrix(sizingMethod=\'auto expand\', M11=' +
+		costheta + ', M12=' + (-sintheta) + ', M21=' + sintheta + ', M22=' + costheta + ')';
+	}
+  }
+});
+
+L.rotatedMarker = function(pos, options) {
+	return new L.RotatedMarker(pos, options);
+};
+
+// mapbox draggable marker example
+// https://www.mapbox.com/mapbox.js/example/v1.0.0/draggable-marker/
+var marker = L.rotatedMarker(new L.LatLng(pjson[0].y, pjson[0].x), {
+	icon: L.icon({
+	iconUrl: 'north.png',
+	iconSize: [37, 37],
+	}),
+	draggable: true
+}).addTo(map);
+
+marker.on('dragend', function(event) {
+	var marker = event.target;
+	var result = marker.getLatLng();
+	video.get(0).currentTime = nearest_point(result.lat, result.lng).sec;
+});
+
+function set_marker(point) {
+	marker.setLatLng(L.latLng(point.y, point.x));
+	set_marker.now = point;
+	marker.options.angle = point.dir;
+}
+set_marker.now = pjson[0];
+
+// handle for marker location based on video time
+function point_at_time(timeindex_sec) {
+	var time_index = pjson[0];
+	var min_time = Number.POSITIVE_INFINITY;
+	for (var i = 0, e = pjson.length; i < e; ++i) {
+		var this_time = Math.abs(pjson[i].sec - timeindex_sec);
+		if (this_time < min_time) {
+			time_index = pjson[i];
+			min_time = this_time;
+		}
+	}
+	return time_index;
+}
+
+// listeners on the video used to place marker
+video.on('seeked', function () {
+	set_marker(point_at_time(this.currentTime));
+});
+
+video.on('timeupdate', function() {
+	var time_index = point_at_time(this.currentTime);
+	if (time_index !== set_marker.now) {
+		set_marker(time_index);
+	}
+});
+
+video.on('ended', function() {
+	set_marker(pjson[pjson.length-1]);
+});
+
+function lineDistance(x1, y1, x2, y2) {
+	var xs = 0, ys = 0;
+	xs = x2 - x1;
+	xs = xs * xs;
+	ys = y2 - y1;
+	ys = ys * ys;
+	return Math.sqrt(xs + ys);
+}
+
+function nearest_point(lat, lng) {
+	var dist = Number.POSITIVE_INFINITY;
+	var time_index;
+	for (var i = 0, e = pjson.length; i < e; ++i) {
+		var this_distance = lineDistance(lng.toString(), lat.toString(), pjson[i].x, pjson[i].y);
+		if (this_distance < dist) {
+			dist = this_distance
+			time_index = pjson[i];
+		}
+	}
+	return time_index;
+}
+
+// map listener to update timeindex associated with nearest point
+map.on('click', function (e) {
+	video.get(0).currentTime = nearest_point(e.latlng.lat, e.latlng.lng).sec;
+});
